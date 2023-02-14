74918e8aafc86af264d702bc3428e91f08195b3f
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-02-14 20:15:16 +0800

    fix: bring naming consistency to NeovimRpcNotificationTyped

--- lib/src/types/nvim_rpc_notification_typed.dart
+++ lib/src/types/nvim_rpc_notification_typed.dart
@@ -11,44 +11,44 @@ abstract class NvimRpcNotificationTyped {
     List<dynamic> params,
   ) {
     if (method == 'redraw') {
-      return NeovimRpcNotification$Redraw.parse(params);
+      return NvimRpcNotification$Redraw.parse(params);
     } else {
-      return NeovimRpcNotification$$$Unknown(method, params);
+      return NvimRpcNotification$$$Unknown(method, params);
     }
   }
 
   R when<R>({
-    required R Function(NeovimRpcNotification$Redraw redraw) redraw,
-    required R Function(NeovimRpcNotification$$$Unknown unknown) $$unknown,
+    required R Function(NvimRpcNotification$Redraw redraw) redraw,
+    required R Function(NvimRpcNotification$$$Unknown unknown) $$unknown,
   });
 
   R maybeWhen<R>({
     required R Function(NvimRpcNotificationTyped notification) orElse,
-    R Function(NeovimRpcNotification$Redraw redraw)? redraw,
-    R Function(NeovimRpcNotification$$$Unknown unknown)? $$unknown,
+    R Function(NvimRpcNotification$Redraw redraw)? redraw,
+    R Function(NvimRpcNotification$$$Unknown unknown)? $$unknown,
   });
 }
 
 /// Represents a RPC notification from Neovim to the client that has failed to type narrow.
-class NeovimRpcNotification$$$Unknown extends NvimRpcNotificationTyped {
+class NvimRpcNotification$$$Unknown extends NvimRpcNotificationTyped {
   final String method;
   final List params;
 
-  const NeovimRpcNotification$$$Unknown(this.method, this.params);
+  const NvimRpcNotification$$$Unknown(this.method, this.params);
 
   @override
   R maybeWhen<R>({
     required R Function(NvimRpcNotificationTyped notification) orElse,
-    R Function(NeovimRpcNotification$Redraw redraw)? redraw,
-    R Function(NeovimRpcNotification$$$Unknown unknown)? $$unknown,
+    R Function(NvimRpcNotification$Redraw redraw)? redraw,
+    R Function(NvimRpcNotification$$$Unknown unknown)? $$unknown,
   }) {
     return $$unknown != null ? $$unknown(this) : orElse(this);
   }
 
   @override
   R when<R>({
-    required R Function(NeovimRpcNotification$Redraw redraw) redraw,
-    required R Function(NeovimRpcNotification$$$Unknown unknown) $$unknown,
+    required R Function(NvimRpcNotification$Redraw redraw) redraw,
+    required R Function(NvimRpcNotification$$$Unknown unknown) $$unknown,
   }) {
     return $$unknown(this);
   }
@@ -58,12 +58,12 @@ class NeovimRpcNotification$$$Unknown extends NvimRpcNotificationTyped {
 /// Contains a batch of UI events in [events].
 ///
 /// See [NeovimUIEvent] for more details
-class NeovimRpcNotification$Redraw extends NvimRpcNotificationTyped {
+class NvimRpcNotification$Redraw extends NvimRpcNotificationTyped {
   final List<NvimUIEvent> events;
 
-  const NeovimRpcNotification$Redraw(this.events);
+  const NvimRpcNotification$Redraw(this.events);
 
-  factory NeovimRpcNotification$Redraw.parse(List<dynamic> rawEvents) {
+  factory NvimRpcNotification$Redraw.parse(List<dynamic> rawEvents) {
     final unpackedEvents = List.generate(rawEvents.length, (index) {
       final rawEvent = rawEvents[index] as List<dynamic>;
       final eventName = rawEvent[0] as String;
@@ -76,21 +76,21 @@ class NeovimRpcNotification$Redraw extends NvimRpcNotificationTyped {
     final flattenedEvents = [
       for (final batchedEvents in unpackedEvents) ...batchedEvents
     ];
-    return NeovimRpcNotification$Redraw(flattenedEvents);
+    return NvimRpcNotification$Redraw(flattenedEvents);
   }
 
   @override
   R maybeWhen<R>(
       {required R Function(NvimRpcNotificationTyped notification) orElse,
-      R Function(NeovimRpcNotification$Redraw redraw)? redraw,
-      R Function(NeovimRpcNotification$$$Unknown unknown)? $$unknown}) {
+      R Function(NvimRpcNotification$Redraw redraw)? redraw,
+      R Function(NvimRpcNotification$$$Unknown unknown)? $$unknown}) {
     return redraw != null ? redraw(this) : orElse(this);
   }
 
   @override
   R when<R>({
-    required R Function(NeovimRpcNotification$Redraw redraw) redraw,
-    required R Function(NeovimRpcNotification$$$Unknown unknown) $$unknown,
+    required R Function(NvimRpcNotification$Redraw redraw) redraw,
+    required R Function(NvimRpcNotification$$$Unknown unknown) $$unknown,
   }) {
     return redraw(this);
   }
