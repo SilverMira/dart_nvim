ffab30dea7e6a4e1043def5f1cd8aa0d0f0e2420
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-09-03 19:32:04 +0800

    chore: remove old files

--- lib/dart_nvim.dart
+++ lib/dart_nvim.dart
@@ -1,6 +1,7 @@
 library dart_nvim;
 
 export 'package:dart_nvim/src/base/nvim.dart';
+export 'package:dart_nvim/src/base/api.dart';
 export 'package:dart_nvim/src/class/rpc_error.dart';
 export 'package:dart_nvim/src/types/nvim_error.dart';
 export 'package:dart_nvim/src/types/nvim_ext.dart';
