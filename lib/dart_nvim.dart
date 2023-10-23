763450714b2e138d5dc7fdb427510c334abad7d8
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-10-23 23:18:39 +0800

    feat: expose rpc_request and rpc_notification

--- lib/dart_nvim.dart
+++ lib/dart_nvim.dart
@@ -5,3 +5,5 @@ export 'package:dart_nvim/src/base/api.dart';
 export 'package:dart_nvim/src/class/rpc_error.dart';
 export 'package:dart_nvim/src/types/nvim_error.dart';
 export 'package:dart_nvim/src/types/nvim_ext.dart';
+export 'package:dart_nvim/src/class/rpc_notification.dart';
+export 'package:dart_nvim/src/class/rpc_request.dart';
